f7329d846d2be851d73c5f01f110b14fdaffb700
Alexa Xu <xby04152002@163.com>
2024-11-04 15:01:29 -0500

    final .bff file reader

--- read_bff.py
+++ read_bff.py
@@ -7,6 +7,7 @@ A .bff file includes the board, the number of blocks, and the lasers setting.
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import time
 
 def readf_bff(file_path):
     '''
@@ -33,9 +34,12 @@ def readf_bff(file_path):
         lazor_position = []
         lazor_direction = []
         target = []
+        grid = []
 
+        # read the content of the file and split it by lines
         bff = open(file_path, 'r').read().splitlines()
 
+        # read the grid of the game, from "GRID START" to "GRID STOP"
         for i in range(len(bff)):
             if bff[i]=="GRID START":
                 for j in range(i+1, len(bff)):
@@ -43,57 +47,82 @@ def readf_bff(file_path):
                         break
                     else:
                         board.append(bff[j])
+        # read the number of each blocks
             elif len(bff[i]) == 3 and bff[i][0] == "A":
                 A_num = int(bff[i][2])
             elif len(bff[i]) == 3 and bff[i][0]=="B":
                 B_num = int(bff[i][2])
             elif len(bff[i]) == 3 and bff[i][0]=="C":
                 C_num = int(bff[i][2])
+        # read the lazor and target points
             elif len(bff[i]) != 0 and bff[i][0]=="L":
                 lazor = bff[i].split(" ")
                 lazor_position.append([int(lazor[1]), int(lazor[2])])
                 lazor_direction.append([int(lazor[3]), int(lazor[4])])
+        # read the target points
             elif len(bff[i]) != 0 and bff[i][0]=="P":
                 target.append([int(bff[i][2]), int(bff[i][4])])
-
+    # convert the board to the 2D list
     for x in board:
         lists = x.split()
-        original_board.append(lists)
- 
-    return original_board, A_num, B_num, C_num, lazor_position, lazor_direction, target
+        original_board.append(lists) 
 
-def convert_to_grid(borad):
-    '''
-    This function is used to convert the board to the grid.
-    
-    ***Parameters***
-    board: 2D list, the board of the game.
-    permut: list, the permutation of the blocks.
-    ***Return***
-    grid: 2D list, the grid of the game
-    '''
-    n = len(board)
-    m = len(board[0])
-    
-    # 创建一个 (2n + 1) x (2m + 1) 的网格，初始化为 'x'
-    grid = [['1' for _ in range(2 * m + 1)] for _ in range(2 * n + 1)]
-    
+    # convert the board to the grid, fulfill the empty space with 'x'
+    # because the step is half of the block, so the size of the grid is 2*len+1
+    n = len(original_board)
+    m = len(original_board[0])
+    grid = [['x' for _ in range(2 * m + 1)] for _ in range(2 * n + 1)]
     for i in range(n):
         for j in range(m):
-            # 将棋盘元素放置在网格的适当位置
-            grid[2 * i + 1][2 * j + 1] = board[i][j]
-    
-    return grid
+            grid[2 * i + 1][2 * j + 1] = original_board[i][j]
 
-if __name__ == "__main__":
-    board, A_num, B_num, C_num, lazor_position, lazor_direction, target = readf_bff("C:/Users/administer/Desktop/SC/Lazor_Project/bff_files/dark_1.bff")
-    print(board)
-    print(A_num)
-    print(B_num)
-    print(C_num)
-    print(lazor_position)
-    print(lazor_direction)
-    print(target)
-    grid = convert_to_grid(board)
-    for row in grid:
-        print(' '.join(row))
+    # check if the grid is valid
+    valid_blocks = {'x', 'o', 'A', 'B', 'C'}
+    for i, row in enumerate(original_board):
+        for j, char in enumerate(row):
+            if char not in valid_blocks:
+                raise Exception(f'Invalid character "{char}" at ({j}, {i}) in the grid!')
+            
+    # check if the blocks are valid - category A, B, C & nums
+    if A_num==0 and B_num==0 and C_num == 0:
+        print("There is no usable blocks! Please check the .bff file.")
+    if A_num < 0 or B_num < 0 or C_num < 0:
+        print("Invalid block number! Please check the .bff file.")
+    if A_num + B_num + C_num > n * m:
+        print("The number of blocks is greater than the grid size! Please check the .bff file.")
+
+    # check if the lasers are valid
+    if lazor == 0:
+        print("There is no lazor! Please check the .bff file.")
+    for i in range(len(lazor)):
+        if lazor[i] !=4:
+            print("Invalid lazor setting! Please check the .bff file.")
+    for i in range(len(lazor_position)):
+        if not (lazor_position[i][0] in [0, 2 * n] and lazor_position[i][1] in [0, 2 * m]):
+            print("Invalid lazor position! Please check the .bff file.")
+        if not (lazor_direction[i][0] in [-1, 1] and lazor_direction[i][1] in [-1, 1]):
+            print("Invalid lazor direction! Please check the .bff file.")
+
+    # check if the target points are valid
+    if target == 0:
+        print("There is no target point! Please check the .bff file.")
+    for i in range(len(target)):
+        if not (target[i][0] in [0, 2 * n] and target[i][1] in [0, 2 * m]):
+            print("Invalid target point! Please check the .bff file.")
+
+    return original_board, grid, A_num, B_num, C_num, lazor_position, lazor_direction, target
+
+
+# if __name__ == "__main__":
+#     time1 = time.time()
+#     original_board, grid, A_num, B_num, C_num, lazor_position, lazor_direction, target = readf_bff("C:/Users/administer/Desktop/SC/lazor/Lazors-Project/mad_1.bff")
+#     print(original_board)
+#     print(grid)
+#     print(A_num)
+#     print(B_num)
+#     print(C_num)
+#     print(lazor_position)
+#     print(lazor_direction)
+#     print(target)
+#     time2 = time.time()
+#     print("The time used is: ", time2-time1)
