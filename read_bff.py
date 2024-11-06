07091f5979acf578640af5db2c13794de5f7d49c
Alexa Xu <xby04152002@163.com>
2024-11-06 18:18:35 -0500

    Final .bff reader

--- read_bff.py
+++ read_bff.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import time
+from utlis import check_if_the_point_in_grid
 
 def readf_bff(file_path):
     '''
@@ -57,8 +58,11 @@ def readf_bff(file_path):
         # read the lazor and target points
             elif len(bff[i]) != 0 and bff[i][0]=="L":
                 lazor = bff[i].split(" ")
-                lazor_position.append([int(lazor[1]), int(lazor[2])])
-                lazor_direction.append([int(lazor[3]), int(lazor[4])])
+                if len(lazor) == 5:
+                    lazor_position.append([int(lazor[1]), int(lazor[2])])
+                    lazor_direction.append([int(lazor[3]), int(lazor[4])])
+                else:
+                    print("Invalid lazor setting! Please check the .bff file.")
         # read the target points
             elif len(bff[i]) != 0 and bff[i][0]=="P":
                 target.append([int(bff[i][2]), int(bff[i][4])])
@@ -76,53 +80,45 @@ def readf_bff(file_path):
         for j in range(m):
             grid[2 * i + 1][2 * j + 1] = original_board[i][j]
 
-    # check if the grid is valid
-    valid_blocks = {'x', 'o', 'A', 'B', 'C'}
-    for i, row in enumerate(original_board):
-        for j, char in enumerate(row):
-            if char not in valid_blocks:
-                raise Exception(f'Invalid character "{char}" at ({j}, {i}) in the grid!')
-            
-    # check if the blocks are valid - category A, B, C & nums
-    if A_num==0 and B_num==0 and C_num == 0:
-        print("There is no usable blocks! Please check the .bff file.")
-    if A_num < 0 or B_num < 0 or C_num < 0:
-        print("Invalid block number! Please check the .bff file.")
-    if A_num + B_num + C_num > n * m:
-        print("The number of blocks is greater than the grid size! Please check the .bff file.")
-
-    # check if the lasers are valid
-    if lazor == 0:
-        print("There is no lazor! Please check the .bff file.")
-    for i in range(len(lazor)):
-        if lazor[i] !=4:
-            print("Invalid lazor setting! Please check the .bff file.")
+    # check if the settings are valid 
+    row = len(grid)
+    column = len(grid[0])
+    # check the laser settings
+    if len(lazor) == 0:
+        print('No laser! Please check the .bff file.')
+    # check the laser position and direction
     for i in range(len(lazor_position)):
-        if not (lazor_position[i][0] in [0, 2 * n] and lazor_position[i][1] in [0, 2 * m]):
-            print("Invalid lazor position! Please check the .bff file.")
+        if check_if_the_point_in_grid(lazor_position[i], grid) == False:
+            print(f'Laser {i} starts out of the grid! Please check the .bff file.')
         if not (lazor_direction[i][0] in [-1, 1] and lazor_direction[i][1] in [-1, 1]):
-            print("Invalid lazor direction! Please check the .bff file.")
+            print(f'Invalid laser direction of laser {i}! Please check the .bff file.')
 
-    # check if the target points are valid
-    if target == 0:
-        print("There is no target point! Please check the .bff file.")
+    # check the target points
+    if len(target) == 0:
+        print('No target point! Please check the .bff file.')
     for i in range(len(target)):
-        if not (target[i][0] in [0, 2 * n] and target[i][1] in [0, 2 * m]):
-            print("Invalid target point! Please check the .bff file.")
+        if check_if_the_point_in_grid(target[i], grid) == False:
+            print(f'Target point {i} is out of the grid! Please check the .bff file.')
+    
+    # check the block settings
+    if (A_num + B_num + C_num) == 0:
+        print('No usable blocks! Please check the .bff file.')
+    if A_num < 0 or B_num < 0 or C_num < 0:
+        print('Invalid block number! Please check the .bff file.')
+    if (A_num + B_num + C_num) >= row * column:
+        print('The number of blocks is greater than the grid size! Please check the .bff file.')
+
+    # check the grid settings
+    valid_chars = {'x', 'o', 'A', 'B', 'C'}
+    for i in range(row):
+        for j in range(column):
+            if grid[i][j] not in valid_chars:
+                print(f'Invalid character "{grid[i][j]}" at ({j}, {i}) in the grid!')
 
     return original_board, grid, A_num, B_num, C_num, lazor_position, lazor_direction, target
 
 
 # if __name__ == "__main__":
-#     time1 = time.time()
-#     original_board, grid, A_num, B_num, C_num, lazor_position, lazor_direction, target = readf_bff("C:/Users/administer/Desktop/SC/lazor/Lazors-Project/mad_1.bff")
-#     print(original_board)
-#     print(grid)
-#     print(A_num)
-#     print(B_num)
-#     print(C_num)
-#     print(lazor_position)
-#     print(lazor_direction)
-#     print(target)
-#     time2 = time.time()
-#     print("The time used is: ", time2-time1)
+    
+#     data = readf_bff("C:/Users/administer/Desktop/SC/Lazor_Project/bff_files/mad_1.bff")
+#     print(data)
