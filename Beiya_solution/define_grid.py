b991ffde7d49bf3a9bfd213886cd3436a7aac2a4
Alexa Xu <xby04152002@163.com>
2024-11-09 16:44:58 -0500

    update grid class to read more clearly

--- Beiya_solution/define_grid.py
+++ Beiya_solution/define_grid.py
@@ -1,16 +1,25 @@
-import numpy as np
-import pandas as pd
-import matplotlib as plt
-
+'''
+This file defines the grid class, which is used to generate the grid based on the original grid and the position of the obstacles.
+'''
 class Grid(object):
-
     def __init__(self, original_grid):
+        '''
+        This function initializes the grid class.
+        '''
         self.list_grid = None
         self.length = len(original_grid)
         self.width = len(original_grid[0])
         self.original_grid = original_grid
 
     def generate_grid(self, list_grid, position):
+        '''
+        This function generates the grid based on the original grid and the position of the obstacles.
+        ***Parameters***
+        list_grid: list, the list of blocks in the grid.
+        position: list, the position of the obstacles.
+        ***Return***
+        original_grid: 2D list, the generated grid.
+        '''
         self.list_grid = list_grid
         for row in range(len(self.original_grid)):
             for column in range(len(self.original_grid[0])):
